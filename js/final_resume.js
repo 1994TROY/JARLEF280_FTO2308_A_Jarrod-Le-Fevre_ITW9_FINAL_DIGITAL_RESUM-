f491cd83a4108c6deafa12c278cfcdb905032ea0
Jarrod <144126463+1994TROY@users.noreply.github.com>
2023-09-28 23:23:39 +0200

    Finished my About Section, with styling/responsive

--- js/final_resume.js
+++
@@ -1,6 +0,0 @@
-const mobileMenuButton = document.getElementById("mobile-menu");
-const navList = document.querySelector(".navbar ul");
-
-mobileMenuButton.addEventListener("click", function () {
-  navList.classList.toggle("active");
-});
